96f73cb8e8375b1b071393b8c2c0a04c6baa5e18
YashPPatel <yp7121@gmail.com>
2021-06-07 19:09:39 +0530

    update

--- datacrate/src/components/Form.js
+++ datacrate/src/components/Form.js
@@ -2,7 +2,9 @@ import React, { useState } from 'react';
 import FormSignup from './FormSignup';
 import FormSuccess from './FormSuccess';
 import signup from '../assets/signup.jpg'
+import img1 from '../assets/mobileimg.jpg'
 import './Form.css'
+import '../App.css'
 
 const Form = () => {
   const [isSubmitted, setIsSubmitted] = useState(false);
@@ -13,17 +15,20 @@ const Form = () => {
   return (
     <>
     
+    
       <div className='form-container'>
         <span className='close-btn'>×</span>
         <div className='form-content-left'>
-        <h1>hello</h1>
-           <img className='form-img' src={signup} /> 
+           <img className='form-img' src={signup} />
+           <img className='form-mobimg' src={img1} />
+
         </div>
         {!isSubmitted ? 
           <FormSignup submitForm={submitForm} />
          : 
           <FormSuccess />
         }
+      
       </div>
       
       
--- datacrate/src/components/FormSignup.js
+++ datacrate/src/components/FormSignup.js
@@ -7,31 +7,29 @@ import './Footer.css'
 const FormSignup = ({ submitForm }) => {
     const { handleChange, values, handleSubmit, errors } = useForm(submitForm, validate)
     return (
+        
         <div className="form-content-right">
             <form className="form" onSubmit={handleSubmit}>
                 <h1>Get started with us!</h1>
                 <div className="form-inputs">
                     <label htmlFor="username" className="form-label">Username</label>
                     <input type="text" name="username" id="username" placeholder="Enter Username" className="form-input" value={values.username} onChange={handleChange} />
-                    {errors.username && <p>{errors.username}</p>}
+                    
 
                 </div>
                 <div className="form-inputs">
                     <label htmlFor="email" className="form-label">Email</label>
                     <input type="text" name="email" id="email" placeholder="Enter email" className="form-input" value={values.email} onChange={handleChange} />
-                    {errors.email && <p>{errors.email}</p>}
-                </div>
+                            </div>
                 <div className="form-inputs">
                     <label htmlFor="password" className="form-label">Password</label>
                     <input type="password" name="password" id="password" placeholder="Enter Password" className="form-input" value={values.password} onChange={handleChange} />
-                    {errors.password && <p>{errors.password}</p>}
-                </div>
+                                </div>
                 <div className="form-inputs">
                     <label htmlFor="cpassword" className="form-label">Confirm Password</label>
                     <input type="password" name="cpassword" id="cpassword" placeholder="Enter Password Again" className="form-input" value={values.cpassword} onChange={handleChange} />
-                    {errors.cpassword && <p>{errors.cpassword}</p>}
-                </div>
-                <button className="form-input-btn" type="submit">Sign Up</button>
+                                    </div>
+                        <div> <button className="form-input-btn" type="submit">Sign Up</button></div>
 
                 <div className="form-inputs">
                     <h3>-------- Or Signup with --------</h3>
