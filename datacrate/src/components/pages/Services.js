31753bea1b338a864bc66ddcf39d37f6dfca0a92
YashPPatel <yp7121@gmail.com>
2021-06-09 12:07:59 +0530

    update

--- datacrate/src/components/pages/Services.js
+++ datacrate/src/components/pages/Services.js
@@ -1,15 +1,78 @@
 import React from 'react'
+import ScriptTag from 'react-script-tag';
 import './Services.css'
 import gold from '../../assets/i12-gold.png'
 import graphite from '../../assets/i12-graphite.png'
 import pacific from '../../assets/i12-pacificblue.png'
 import silver from '../../assets/i12-silver.png'
 import alogo from '../../assets/alogo.png'
-import './Servicesupdate'
+
 
 export default function Services() {
+  
+            const colors = document.querySelectorAll('.color');
+            const phones = document.querySelectorAll('.phone');
+            const gradients = document.querySelectorAll('.gradient');
+            //const phoneBg = document.querySelector('.phonebackground');
+
+            let prevColor = "gold";
+            let animationEnd = true;
+
+            
+            
+            function changeColor(){
+                if(!animationEnd) return;
+                let primary = this.getAttribute('primary');
+                let color = this.getAttribute('color');
+                let phone = document.querySelector(`.phone[color="${color}"]`);
+                let gradient = document.querySelector(`.gradient[color="${color}"]`);
+                let prevGradient = document.querySelector(`.gradient[color="${prevColor}"]`);
+            
+                if(color === prevColor) return;
+            
+                colors.forEach(c => c.classList.remove('active'));
+                this.classList.add('active');
+            
+                document.documentElement.style.setProperty('--primary', primary);
+                
+                phones.forEach(s => s.classList.remove('show'));
+                phone.classList.add('show');
+            
+                gradients.forEach(g => g.classList.remove('first', 'second'));
+                gradient.classList.add('first');
+                prevGradient.classList.add('second');
+            
+                prevColor = color;
+                animationEnd = false;
+            
+                gradient.addEventListener('animationend', () => {
+                    animationEnd = true;
+                })
+            }
+            
+            colors.forEach(c => c.addEventListener('click', changeColor));
+            
+            
+            // let x = window.matchMedia("(max-width: 1000px)");
+            
+            // function changeHeight(){
+            //     if(x.matches){
+            //       console.warn(phones[0])
+            //         let phoneHeight = phones[0].offsetHeight;
+            //         console.log(phoneHeight)
+            //         phoneBg.style.height = `${phoneHeight * 0.9}px`;
+            //     }
+            //     else{
+            //         phoneBg.style.height = "475px";
+            //     }
+            // }
+            
+            // changeHeight();
+            
+            // window.addEventListener('resize', changeHeight)
+          
   return (
-    <>
+    <div>
       <div class="containerservice">
         <div class="card">
           <div class="phonebackground">
@@ -43,7 +106,7 @@ export default function Services() {
                           <p class="text">5G speed. A14 Bionic, the fastest chip in a smartphone. And a Pro camera system that takes low-light photography to the next level - with an even bigger jump on iphone 12 Pro Max.</p>
                         </div>
                         <div class="color-container">
-                          <h3 class="title">Color</h3>
+                          <h3 class="title">Colors</h3>
                           <div class="colors">
                             <span class="color active" primary="#fdeed9" color="gold"></span>
                             <span class="color" primary="#656160" color="graphite"></span>
@@ -63,6 +126,6 @@ export default function Services() {
                       </div>
         </div>
     </div>  
-        </>
+        </div>
    )
 }
